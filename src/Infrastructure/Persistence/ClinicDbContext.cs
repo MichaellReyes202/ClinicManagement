5b47f186b974096a1c079b21e14a6dffc0cf013e
Michaell Joel Reyes <michaelljoel.reyes202@gmail.com>
2025-09-05 20:59:55 -0600

    delete

--- src/Infrastructure/Persistence/ClinicDbContext.cs
+++ src/Infrastructure/Persistence/ClinicDbContext.cs
@@ -7,140 +7,138 @@ namespace Infrastructure.Persistence;
 
 public partial class ClinicDbContext : DbContext
 {
-    public ClinicDbContext()
+  public ClinicDbContext()
+  {
+  }
+
+  public ClinicDbContext(DbContextOptions<ClinicDbContext> options)
+      : base(options)
+  {
+  }
+
+  public virtual DbSet<Role> Roles { get; set; }
+
+  public virtual DbSet<User> Users { get; set; }
+
+  public virtual DbSet<UserRole> UserRoles { get; set; }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql("");
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder
+        .HasPostgresExtension("pg_trgm")
+        .HasPostgresExtension("uuid-ossp");
+
+    modelBuilder.Entity<Role>(entity =>
     {
-    }
+      entity.HasKey(e => e.Id).HasName("roles_pkey");
 
-    public ClinicDbContext(DbContextOptions<ClinicDbContext> options)
-        : base(options)
+      entity.ToTable("roles");
+
+      entity.HasIndex(e => e.Name, "roles_name_key").IsUnique();
+
+      entity.Property(e => e.Id).HasColumnName("id");
+      entity.Property(e => e.CreatedAt)
+              .HasDefaultValueSql("CURRENT_TIMESTAMP")
+              .HasColumnType("timestamp without time zone")
+              .HasColumnName("created_at");
+      entity.Property(e => e.Description).HasColumnName("description");
+      entity.Property(e => e.Name)
+              .HasMaxLength(50)
+              .HasColumnName("name");
+      entity.Property(e => e.UpdatedAt)
+              .HasDefaultValueSql("CURRENT_TIMESTAMP")
+              .HasColumnType("timestamp without time zone")
+              .HasColumnName("updated_at");
+    });
+
+    modelBuilder.Entity<User>(entity =>
     {
-    }
+      entity.HasKey(e => e.Id).HasName("users_pkey");
 
-    public virtual DbSet<Role> Roles { get; set; }
+      entity.ToTable("users");
 
-    public virtual DbSet<User> Users { get; set; }
+      entity.HasIndex(e => e.Email, "idx_users_email");
 
-    public virtual DbSet<UserRole> UserRoles { get; set; }
+      entity.HasIndex(e => e.NormalizedEmail, "idx_users_normalized_email");
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=ep-blue-tree-aed8hhdt-pooler.c-2.us-east-2.aws.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_jGxmaYBT1QN6;SSL Mode=Require");
+      entity.HasIndex(e => e.Email, "users_email_key").IsUnique();
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+      entity.HasIndex(e => e.NormalizedEmail, "users_normalized_email_key").IsUnique();
+
+      entity.Property(e => e.Id).HasColumnName("id");
+      entity.Property(e => e.CreatedAt)
+              .HasDefaultValueSql("CURRENT_TIMESTAMP")
+              .HasColumnType("timestamp without time zone")
+              .HasColumnName("created_at");
+      entity.Property(e => e.CreatedByUserId).HasColumnName("created_by_user_id");
+      entity.Property(e => e.Email)
+              .HasMaxLength(255)
+              .HasColumnName("email");
+      entity.Property(e => e.IsActive)
+              .HasDefaultValue(true)
+              .HasColumnName("is_active");
+      entity.Property(e => e.LastLogin)
+              .HasColumnType("timestamp without time zone")
+              .HasColumnName("last_login");
+      entity.Property(e => e.NormalizedEmail)
+              .HasColumnType("character varying")
+              .HasColumnName("normalized_email");
+      entity.Property(e => e.PasswordHash)
+              .HasMaxLength(255)
+              .HasColumnName("password_hash");
+      entity.Property(e => e.UpdatedAt)
+              .HasDefaultValueSql("CURRENT_TIMESTAMP")
+              .HasColumnType("timestamp without time zone")
+              .HasColumnName("updated_at");
+      entity.Property(e => e.UpdatedByUserId).HasColumnName("updated_by_user_id");
+
+      entity.HasOne(d => d.CreatedByUser).WithMany(p => p.InverseCreatedByUser)
+              .HasForeignKey(d => d.CreatedByUserId)
+              .OnDelete(DeleteBehavior.SetNull)
+              .HasConstraintName("users_created_by_user_id_fkey");
+
+      entity.HasOne(d => d.UpdatedByUser).WithMany(p => p.InverseUpdatedByUser)
+              .HasForeignKey(d => d.UpdatedByUserId)
+              .OnDelete(DeleteBehavior.SetNull)
+              .HasConstraintName("users_updated_by_user_id_fkey");
+    });
+
+    modelBuilder.Entity<UserRole>(entity =>
     {
-        modelBuilder
-            .HasPostgresExtension("pg_trgm")
-            .HasPostgresExtension("uuid-ossp");
+      entity.HasKey(e => new { e.UserId, e.RoleId }).HasName("user_roles_pkey");
 
-        modelBuilder.Entity<Role>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("roles_pkey");
+      entity.ToTable("user_roles");
 
-            entity.ToTable("roles");
+      entity.HasIndex(e => e.RoleId, "idx_user_roles_role_id");
 
-            entity.HasIndex(e => e.Name, "roles_name_key").IsUnique();
+      entity.HasIndex(e => e.UserId, "idx_user_roles_user_id");
 
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.CreatedAt)
-                .HasDefaultValueSql("CURRENT_TIMESTAMP")
-                .HasColumnType("timestamp without time zone")
-                .HasColumnName("created_at");
-            entity.Property(e => e.Description).HasColumnName("description");
-            entity.Property(e => e.Name)
-                .HasMaxLength(50)
-                .HasColumnName("name");
-            entity.Property(e => e.UpdatedAt)
-                .HasDefaultValueSql("CURRENT_TIMESTAMP")
-                .HasColumnType("timestamp without time zone")
-                .HasColumnName("updated_at");
-        });
+      entity.Property(e => e.UserId).HasColumnName("user_id");
+      entity.Property(e => e.RoleId).HasColumnName("role_id");
+      entity.Property(e => e.CreatedAt)
+              .HasDefaultValueSql("CURRENT_TIMESTAMP")
+              .HasColumnType("timestamp without time zone")
+              .HasColumnName("created_at");
+      entity.Property(e => e.CreatedByUserId).HasColumnName("created_by_user_id");
 
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("users_pkey");
+      entity.HasOne(d => d.CreatedByUser).WithMany(p => p.UserRoleCreatedByUsers)
+              .HasForeignKey(d => d.CreatedByUserId)
+              .OnDelete(DeleteBehavior.SetNull)
+              .HasConstraintName("user_roles_created_by_user_id_fkey");
 
-            entity.ToTable("users");
+      entity.HasOne(d => d.Role).WithMany(p => p.UserRoles)
+              .HasForeignKey(d => d.RoleId)
+              .HasConstraintName("user_roles_role_id_fkey");
 
-            entity.HasIndex(e => e.Email, "idx_users_email");
+      entity.HasOne(d => d.User).WithMany(p => p.UserRoleUsers)
+              .HasForeignKey(d => d.UserId)
+              .HasConstraintName("user_roles_user_id_fkey");
+    });
 
-            entity.HasIndex(e => e.NormalizedEmail, "idx_users_normalized_email");
+    OnModelCreatingPartial(modelBuilder);
+  }
 
-            entity.HasIndex(e => e.Email, "users_email_key").IsUnique();
-
-            entity.HasIndex(e => e.NormalizedEmail, "users_normalized_email_key").IsUnique();
-
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.CreatedAt)
-                .HasDefaultValueSql("CURRENT_TIMESTAMP")
-                .HasColumnType("timestamp without time zone")
-                .HasColumnName("created_at");
-            entity.Property(e => e.CreatedByUserId).HasColumnName("created_by_user_id");
-            entity.Property(e => e.Email)
-                .HasMaxLength(255)
-                .HasColumnName("email");
-            entity.Property(e => e.IsActive)
-                .HasDefaultValue(true)
-                .HasColumnName("is_active");
-            entity.Property(e => e.LastLogin)
-                .HasColumnType("timestamp without time zone")
-                .HasColumnName("last_login");
-            entity.Property(e => e.NormalizedEmail)
-                .HasColumnType("character varying")
-                .HasColumnName("normalized_email");
-            entity.Property(e => e.PasswordHash)
-                .HasMaxLength(255)
-                .HasColumnName("password_hash");
-            entity.Property(e => e.UpdatedAt)
-                .HasDefaultValueSql("CURRENT_TIMESTAMP")
-                .HasColumnType("timestamp without time zone")
-                .HasColumnName("updated_at");
-            entity.Property(e => e.UpdatedByUserId).HasColumnName("updated_by_user_id");
-
-            entity.HasOne(d => d.CreatedByUser).WithMany(p => p.InverseCreatedByUser)
-                .HasForeignKey(d => d.CreatedByUserId)
-                .OnDelete(DeleteBehavior.SetNull)
-                .HasConstraintName("users_created_by_user_id_fkey");
-
-            entity.HasOne(d => d.UpdatedByUser).WithMany(p => p.InverseUpdatedByUser)
-                .HasForeignKey(d => d.UpdatedByUserId)
-                .OnDelete(DeleteBehavior.SetNull)
-                .HasConstraintName("users_updated_by_user_id_fkey");
-        });
-
-        modelBuilder.Entity<UserRole>(entity =>
-        {
-            entity.HasKey(e => new { e.UserId, e.RoleId }).HasName("user_roles_pkey");
-
-            entity.ToTable("user_roles");
-
-            entity.HasIndex(e => e.RoleId, "idx_user_roles_role_id");
-
-            entity.HasIndex(e => e.UserId, "idx_user_roles_user_id");
-
-            entity.Property(e => e.UserId).HasColumnName("user_id");
-            entity.Property(e => e.RoleId).HasColumnName("role_id");
-            entity.Property(e => e.CreatedAt)
-                .HasDefaultValueSql("CURRENT_TIMESTAMP")
-                .HasColumnType("timestamp without time zone")
-                .HasColumnName("created_at");
-            entity.Property(e => e.CreatedByUserId).HasColumnName("created_by_user_id");
-
-            entity.HasOne(d => d.CreatedByUser).WithMany(p => p.UserRoleCreatedByUsers)
-                .HasForeignKey(d => d.CreatedByUserId)
-                .OnDelete(DeleteBehavior.SetNull)
-                .HasConstraintName("user_roles_created_by_user_id_fkey");
-
-            entity.HasOne(d => d.Role).WithMany(p => p.UserRoles)
-                .HasForeignKey(d => d.RoleId)
-                .HasConstraintName("user_roles_role_id_fkey");
-
-            entity.HasOne(d => d.User).WithMany(p => p.UserRoleUsers)
-                .HasForeignKey(d => d.UserId)
-                .HasConstraintName("user_roles_user_id_fkey");
-        });
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+  partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
